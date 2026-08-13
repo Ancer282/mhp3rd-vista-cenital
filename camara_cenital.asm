42ce23711b98e28c00c0d9bdc0294d879f6ee897
Ancer282 <121087318+Ancer282@users.noreply.github.com>
2026-08-13 10:03:28 -0500

    Refactor camera handling in camara_cenital.asm

--- camara_cenital.asm
+++ camara_cenital.asm
@@ -12,23 +12,29 @@ main:
 	beq     	at, zero, @cam_0 ; si la cámara es 0 va a @cam_0, si no lo es retorna
     nop
 	
-	lwc1	f2,0x8(v0)
-	j		yaw_hook+8 ; vuelves al código normal
+	lwc1		f2,0x8(v0)
+	j			yaw_hook+8 ; vuelves al código normal
 	nop
-
+	 
 @cam_0:
-	lwc1	f2,0x8(v0)
-	li      at, 0x459F ; pones la cámara arriba
-	sw		at, 0x6(v0)
-	li      at, DISTAN ; eliges la distancia
-	sw		at, 0xa(v0)
-	j		yaw_hook+8
+	bal     	get_pc
+    nop
+get_pc:
+    move    	t8, ra
+	lwc1		f2,0x8(v0)
+	addiu     	at, t8, (DISTAN - get_pc) ;  pones la cámara arriba y eliges la distancia
+	lh      	at, 0x0(at)
+	sw			at, 0x6(v0)
+	j			yaw_hook+8
 	nop
-
+	
+DISTAN:
+  .byte 0x9B,0x45 ; distancia
+.align 4	
 @main_block_end:
 
 .word 2
-.word  yaw_hook
+.word yaw_hook
 .halfword main - @main_block
 .byte 8
 .byte 0
